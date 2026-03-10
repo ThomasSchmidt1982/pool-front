e5b98c412e19990a263d6d602a93ca673e5c0059
Thomas Schmidt <thomas.michel.schmidt@gmail.com>
2026-03-10 10:49:48 +0100

    fix: bug accents

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,11 @@
 import {API} from "./config.js";
 import {getTokenPayload} from "./utils.js";
 
-// si token absent
+// si token absent -> retour index.html
 if (!localStorage.getItem('jwt_token')) window.location.href = '../index.html';
 
-const token = localStorage.getItem('jwt_token');
 
+const token = localStorage.getItem('jwt_token');
 async function loadPoolStatus() {
     try {
         const res = await fetch(`${API}/pool/status`, {
@@ -21,10 +21,11 @@ async function loadPoolStatus() {
     }
 }
 
+// affichage nom + prenom + role de l'utilisateur connecté
 const user = getTokenPayload();
 document.getElementById('user-name').textContent=`${user.firstname} ${user.lastname}`
 document.getElementById('user-role').textContent = user.role;
-
+console.log(user.firstname +" "+ user.lastname)
 
 // logout
 document.getElementById('btn-logout').addEventListener('click', () => {
@@ -32,5 +33,58 @@ document.getElementById('btn-logout').addEventListener('click', () => {
     window.location.href = '../index.html';
 });
 
+// recherche infos users
+async function loadUsers() {
+    const res = await fetch(`${API}/users`, {
+        method: 'GET',
+        headers: {'Authorization': `Bearer ${token}`}
+    });
+if (!res.ok) return;
+    const data = await res.json();
+
+    const tbody = document.querySelector('#users-table tbody');
+
+data.forEach(user=> {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${user.firstname}</td>
+    <td>${user.lastname}</td>
+    <td>${user.email}</td>
+    <td>${user.isActive ? 'Oui' : 'Non'}</td>
+`;
+    tbody.appendChild(tr);
+})
+    document.getElementById('btn-users').classList.remove('d-none');
+}
+
+// recherche infos employees
+async function loadEmployee() {
+    const res = await fetch(`${API}/employees`, {
+        method: 'GET',
+        headers: {'Authorization': `Bearer ${token}`}
+    });
+    if (!res.ok) return;
+    const data = await res.json();
+
+    const tbody = document.querySelector('#employees-table tbody');
+
+    data.forEach(employee => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+    <td>${employee.firstname}</td>
+    <td>${employee.lastname}</td>
+    <td>${employee.email}</td>
+    <td>${employee.isActive ? 'Oui' : 'Non'}</td>
+`;
+        tbody.appendChild(tr);
+    })
+    document.getElementById('btn-employees').classList.remove('d-none');
+}
+
+// Main
 loadPoolStatus();
 setInterval(loadPoolStatus, 15000)
+if(user.role === 'ROLE_ADMIN'){
+    loadUsers()
+    loadEmployee()
+}
--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,9 @@
-
 // extraction user de jwt
 export function getTokenPayload() {
     const token = localStorage.getItem('jwt_token');
     if (!token) return null;
-    const payload = token.split('.')[1];
-    return JSON.parse(atob(payload));
+    const base64 = token.split('.')[1];
+    return JSON.parse(new TextDecoder().decode(
+        Uint8Array.from(atob(base64), c => c.charCodeAt(0))
+    ));
 }
